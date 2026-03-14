61a9a8f0ab6302ff322a438b39015eeeb7b20fdf
quanzee <jannang201@gmail.com>
2026-03-14 17:17:14 +1100

    Updated so other pages can return to home

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -1,13 +1,15 @@
+import {HashLink} from 'react-router-hash-link';
+
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 flex justify-between items-center p-6 bg-white/80 backdrop-blur-md shadow-sm text-sm">
-      <a href="#home" className="text-xl hover:text-gray-500 font-[600]">Janna's Portfolio</a>
+      <a href="/#home" className="text-xl hover:text-gray-500 font-[600]">Janna's Portfolio</a>
       <ul className="flex gap-6">
-        <li><a href="#about" className="hover:text-gray-500 font-[550] scroll-mt-20">About</a></li>
-        <li><a href="#projects" className="hover:text-gray-500 font-[550] scroll-mt-20">Projects</a></li>
-        <li><a href="#skills" className="hover:text-gray-500 font-[550] scroll-mt-20">Skills</a></li>
-        <li><a href="#writing" className="hover:text-gray-500 font-[550] scroll-mt-20">Writing</a></li>
-        <li><a href="#contact" className="hover:text-gray-500 font-[550] scroll-mt-20">Contact</a></li>
+        <li><HashLink smooth to="/#about" className="hover:text-gray-500 font-[550] scroll-mt-20">About</HashLink></li>
+        <li><HashLink smooth to="/#projects" className="hover:text-gray-500 font-[550] scroll-mt-20">Projects</HashLink></li>
+        <li><HashLink smooth to="/#skills" className="hover:text-gray-500 font-[550] scroll-mt-20">Skills</HashLink></li>
+        <li><HashLink smooth to="/#writing" className="hover:text-gray-500 font-[550] scroll-mt-20">Writing</HashLink></li>
+        <li><HashLink smooth to="/#contact" className="hover:text-gray-500 font-[550] scroll-mt-20">Contact</HashLink></li>
       </ul>
     </nav>
   );
