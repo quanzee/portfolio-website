36c28ee9266891479f29cb2f2721ba0f9e713bee
quanzee <jannang201@gmail.com>
2026-03-14 17:09:21 +1100

    Added ScrollToTop component

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProjectsPage from './pages/ProjectsPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+    {/* placing this component here ensures that every time a new page is loaded, the scroll position resets to the top */}
+      <ScrollToTop /> 
       <Routes>
         {/*The main landing page */}
         <Route path="/" element={<Home />} />
