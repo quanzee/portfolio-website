bac3f247d8032a5e5f0e2e00f65907a0c7ec5394
quanzee <jannang201@gmail.com>
2026-03-15 14:09:32 +1100

    Updated copy

--- my-portfolio/src/pages/ProjectsPage.tsx
+++ my-portfolio/src/pages/ProjectsPage.tsx
@@ -7,7 +7,7 @@ const ProjectsPage = () => {
       <section className="container mx-auto px-6 py-24"> 
         <Navbar />
       <h2 className="text-4xl md:text-5xl mb-4">All Projects</h2>
-        <p className="text-lg text-gray-600 mb-12">A full list of projects from hackathons, university, and personal endeavors. Each project represents a unique challenge and learning experience.</p>
+        <p className="text-lg text-gray-600 mb-12">A full list of projects from hackathons, university, and personal endeavors. Click on a project to learn more.</p>
         {/* The Grid Container */}
        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, index) => (
