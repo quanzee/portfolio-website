be0ed7a795cf100ec81e7623f5d49a2667002049
quanzee <jannang201@gmail.com>
2026-03-18 08:38:15 +1100

    Updated project data

--- my-portfolio/src/data/projects.ts
+++ my-portfolio/src/data/projects.ts
@@ -1,13 +1,22 @@
-import upNextLoomi from '../assets/upnext.png';
+import upNext from '../assets/upnext.png';
 import upNextHeroImage from '../assets/upnext_heroImage.png'
+import loomi from '../assets/loomi_icon.jpg';
+import loomiHeroImage from '../assets/loomi_heroImage.png';
+import residentHealthCare from '../assets/residentHealthCare.png';
+import residentHealthCareHeroImage from '../assets/residentHealthCare_heroImage.png';
+import ISM_AdminProject from '../assets/ISM_AdminProject.png';
+import portfolioWebsite from '../assets/portfolioWebsite.png';
+import dataScience from '../assets/dataScience.png';
+import dataScienceHeroImage from '../assets/dataScience_heroImage.png';
+import { data } from 'react-router-dom';
 
 export const projects = [
   {
     id: 1,
     title: 'UpNext',
     description:
-      'An adaptive assignment planner that helps students break assignments into manageable sessions, visualises progress, and produces deadline-aware suggestions.',
-    image: upNextLoomi,
+      'An adaptive assignment planner prototype that helps students break assignments into manageable sessions, visualises progress, and produces deadline-aware suggestions. Won 4th place in the CSIT X SWITCH 2025 Hackathon.',
+    image: upNext,
     heroImage: upNextHeroImage,
     tags: ['React', 'Next.js', 'TypeScript', 'Tailwind CSS', 'Figma'],
     github: 'https://github.com/JovenTung/UpNext.git',
@@ -18,112 +27,61 @@ export const projects = [
     id: 2,
     title: 'Loomi',
     description:
-      'A comprehensive component library with accessibility-first principles and extensive documentation.',
-    image:
-      'https://images.unsplash.com/photo-1771189255245-225dcea3f652?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjcmVhdGl2ZSUyMGRlc2lnbiUyMHByb2plY3R8ZW58MXx8fHwxNzcxODIyMTYwfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-    tags: ['Swift', 'SwiftUI', 'Fgima'],
-    github: '#',
+      'A parental media review iOS app prototype for parents who want to find media with specific positive values for their young children. Completed as part of a 3-week intensive RMIT Apple Foundation Program course.',
+    image: loomi,
+    heroImage: loomiHeroImage,
+    tags: ['Swift', 'SwiftUI', 'Figma'],
+    github: 'https://github.com/quanzee/loomi.git',
     live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
+    contentPath: '/content/loomi.md',
   },
   {
     id: 3,
     title: 'Resident HealthCare System',
     description:
-      'A collaborative task management tool with real-time updates, team workspaces, and productivity insights.',
-    image:
-      'https://images.unsplash.com/photo-1666723043169-22e29545675c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjB3b3Jrc3BhY2UlMjBkZXNrfGVufDF8fHx8MTc3MTc3NDY0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      'A resident healthcare management system with different interfaces for different staff roles in a care home with a fully functioning backend and frontend. Completed as part of an Advanced Programming course.',
+    image: residentHealthCare,
+    heroImage: residentHealthCareHeroImage,
     tags: ['Java', 'SQL'],
-    github: '#',
+    github: 'https://github.com/COSC1295-advanced-programming-2025-s2/s4160608_JannaQianZiNg_Assignment2.git',
     live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
+    contentPath: '/content/residentHealthCare.md'
   },
   {
     id: 4,
     title: 'Administrative Data Management Project',
     description:
-      'A collaborative task management tool with real-time updates, team workspaces, and productivity insights.',
-    image:
-      'https://images.unsplash.com/photo-1666723043169-22e29545675c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjB3b3Jrc3BhY2UlMjBkZXNrfGVufDF8fHx8MTc3MTc3NDY0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      'Assisted a local music school business in managing their administrative data by creating a Google Sheets system for tracking student attendance and teachers\' wages, and a Figma prototype for a potential future web app to manage this data more efficiently.',
+    image: ISM_AdminProject,
+    heroImage: ISM_AdminProject,
     tags: ['Google Sheets', 'Figma'],
-    github: '#',
+    github: 'https://www.figma.com/design/qXqzwA7DkOpFBKfVH7WMGF/ISM-A-W-App?node-id=0-1&t=VTcIvfkD4iaZRoUa-1',
     live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
+    contentPath: '/content/ISM_AdminProject.md'
   },
-    {
-    id: 5,
-    title: 'Data Science Final Report',
-    description:
-      'A collaborative task management tool with real-time updates, team workspaces, and productivity insights.',
-    image:
-      'https://images.unsplash.com/photo-1666723043169-22e29545675c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjB3b3Jrc3BhY2UlMjBkZXNrfGVufDF8fHx8MTc3MTc3NDY0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-    tags: ['Python', 'Data Analysis', 'Machine Learning'],
-    github: '#',
-    live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
-  },
-    {
-    id: 6,
-    title: 'Algorithms and Analaysis Assignment',
-    description:
-      'A collaborative task management tool with real-time updates, team workspaces, and productivity insights.',
-    image:
-      'https://images.unsplash.com/photo-1666723043169-22e29545675c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjB3b3Jrc3BhY2UlMjBkZXNrfGVufDF8fHx8MTc3MTc3NDY0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-    tags: ['Python'],
-    github: '#',
-    live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
-  },
-      {
-    id: 7,
-    title: 'Database Concepts Final Assignment',
-    description:
-      'A collaborative task management tool with real-time updates, team workspaces, and productivity insights.',
-    image:
-      'https://images.unsplash.com/photo-1666723043169-22e29545675c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjB3b3Jrc3BhY2UlMjBkZXNrfGVufDF8fHx8MTc3MTc3NDY0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-    tags: ['SQL', 'SQLite', 'Data Analysis'],
-    github: '#',
-    live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
-  },
-        {
+          {
     id: 7,
     title: 'This Portfolio Website',
     description:
-      'A collaborative task management tool with real-time updates, team workspaces, and productivity insights.',
-    image:
-      'https://images.unsplash.com/photo-1666723043169-22e29545675c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjB3b3Jrc3BhY2UlMjBkZXNrfGVufDF8fHx8MTc3MTc3NDY0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      'The website you are on right now, built from scratch.',
+    image: portfolioWebsite,
+    heroImage: portfolioWebsite,
     tags: ['React', 'Vite', 'TypeScript', 'Tailwind CSS'],
+    github: 'https://github.com/quanzee/portfolio-website.git',
+    live: 'http://localhost:5173/#home',
+    contentPath: '/content/portfolio_website.md'
+  },
+    {
+    id: 5,
+    title: 'Movie Recommender System Assignment',
+    description:
+      'Implemented and evaluated several recommender system algorithms, including user-based and item-based collaborative filtering, matrix factorization, and content-based filtering, using the MovieLens dataset. Completed as part of a Data Science course.',
+    image: dataScience,
+    heroImage: dataScienceHeroImage,
+    tags: ['Python', 'Data Analysis', 'Machine Learning'],
     github: '#',
     live: '#',
-    sections: [
-      { type: 'text', content: 'In this project, I built a town of agents...' },
-      { type: 'image', url: '/project-img.jpg', caption: 'The architecture' },
-      { type: 'text', content: 'The challenge was memory persistence...' }
-    ]
+    contentPath: '/content/movie_recommender_system.md'
   },
+
 ];
