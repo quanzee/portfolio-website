5bf20e924c5490ab15cd73e0af4d0b85341a8472
quanzee <jannang201@gmail.com>
2026-03-18 08:39:09 +1100

    Updated UI to have a 2-grid hero

--- my-portfolio/src/pages/ProjectDetail.tsx
+++ my-portfolio/src/pages/ProjectDetail.tsx
@@ -27,9 +27,26 @@ const ProjectDetail = () => {
     <div className="min-h-screen bg-white">
       <Navbar />
       
-      <div className="flex flex-col gap-5 mt-30 px-6">
-        <h1 className="text-5xl font-bold">{project.title}</h1>
+      <div className="flex flex-col gap-5 mt-10 px-24 py-24 ">
+        <div className="grid md:grid-cols-2 gap-2 bg-gray-100 p-6 rounded-lg">
+
+          <div className="flex flex-col gap-4 mt-auto mb-auto">
+          <h1 className="text-5xl font-bold">{project.title}</h1>
+          
+        {/* Show the tags at the top of the page */}
         <div className="flex gap-4">
+          {project.tags.map((tag) => (
+            <span
+              key={tag}
+              className="text-xs bg-gray-200 text-gray-800 px-2 py-1 rounded-full"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+          <p className="text-gray-600">{project.description}</p>
+
+          <div className="flex gap-6">
           <a
             href={project.github}
             className="flex items-center gap-1 text-sm hover:opacity-70 transition-opacity"
@@ -37,6 +54,8 @@ const ProjectDetail = () => {
                     <Github size={16} />
                     Code
                   </a>
+
+                  {project.live !== '#' && (
                   <a
                     href={project.live}
                     className="flex items-center gap-1 text-sm hover:opacity-70 transition-opacity"
@@ -44,14 +63,20 @@ const ProjectDetail = () => {
                     <ExternalLink size={16} />
                     Live Demo
                   </a>
+                  )}
                 </div>
-                <img src= {project.heroImage} alt={project.title} className="w-full rounded-lg shadow-lg" />
-              </div>
 
-      <article className="prose lg:prose-xl mx-auto px-6">
+                </div>
+                  <img src={project.heroImage} alt={project.title} className="rounded-lg shadow-lg object-cover" />
+        </div>
+
+        <div className="bg-gray-100 p-6 rounded-lg">
+    <article className="prose lg:prose-xl mx-auto px-6">
       {/* This component turns # Heading into <h1>Heading</h1> */}
       <ReactMarkdown>{markdown}</ReactMarkdown>
     </article>
+    </div>
+        </div>
     </div>
   );
 };
